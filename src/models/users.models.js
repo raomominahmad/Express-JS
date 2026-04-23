74cd0c9c989a163defd5d4a5b750988850ab3878
raomominahmad <258462334+raomominahmad@users.noreply.github.com>
2026-04-23 09:13:07 +0500

    Add User Schema and create pre hook and methods for hashing and checking the password

---
+++ src/models/users.models.js
@@ -0,0 +1,80 @@
+import mongoose, { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: "https://placehold.co/200X200",
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    refreshToken: {
+      type: String,
+    },
+
+    forgotPasswordToken: {
+      type: String,
+    },
+
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// 10 is cost factor which means hashing process 2 ^ 10 = 1024 times internally
+// next() means move to next middleware
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+   return await bcrypt.compare(password ,this.password)  // true or false
+}
+
+export const User = mongoose.model("User", userSchema);
