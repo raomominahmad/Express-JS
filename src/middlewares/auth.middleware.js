5d319eed7c13e521652edec905953c3092cb8d69
raomominahmad <258462334+raomominahmad@users.noreply.github.com>
2026-04-26 15:05:15 +0500

    add middleware validateProjectPermission in auth middleware file

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,12 +1,13 @@
 import { User } from "../models/users.models.js";
+import { ProjectMember } from "../models/projectmember.models.js";
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
-
+import mongoose from "mongoose";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-// Get access token either from cookies or from Authorization header (Bearer token)
-    const token =
+  // Get access token either from cookies or from Authorization header (Bearer token)
+  const token =
     req.cookies?.accessToken ||
     req.header("Authorization")?.replace("Bearer ", "");
 
@@ -23,11 +24,41 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     if (!user) {
       throw new ApiError(401, "Invalid access token");
     }
-    
+
     // Attach the authenticated user to the request object so that the next middleware/controller can use it.
     req.user = user;
-    next(); 
+    next();
   } catch (error) {
-    throw new ApiError(401,"Invalid access token")
+    throw new ApiError(401, "Invalid access token");
   }
 });
+
+export const validateProjectPermission = (roles = []) => {
+  return asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw new ApiError(400, "project id is missing");
+    }
+
+    const project = await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user._id),
+    });
+
+    if (!project) {
+      throw new ApiError(400, "Project not found");
+    }
+
+    const givenRole = project?.role;
+
+    req.user.role = givenRole;
+
+    if (!roles.includes(givenRole)) {
+      new ApiError(403, "You do not have permission to perform this action");
+    }
+    
+    next()
+
+  });
+};
