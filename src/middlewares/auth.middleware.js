5a1adf081b905187c2326f175c0dc3380dce0ba4
raomominahmad <258462334+raomominahmad@users.noreply.github.com>
2026-05-03 15:41:48 +0500

    corrected the error throw statement

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -55,7 +55,7 @@ export const validateProjectPermission = (roles = []) => {
     req.user.role = givenRole;
 
     if (!roles.includes(givenRole)) {
-      new ApiError(403, "You do not have permission to perform this action");
+      throw new ApiError(403, "You do not have permission to perform this action");
     }
     
     next()
